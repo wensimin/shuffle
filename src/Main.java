fc728b33df60240fbae4e5e6633f6c4a61b0db70
wensimin <hukaizhiyu2@gmail.com>
2019-07-04 13:49:36 +0800

    完成

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    private static final String[] data = {"a", "i", "u", "e", "o", "ka", "ki", "ku", "ke", "ko", "sa", "si", "su", "se", "so"};
+
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList(data);
+        Collections.shuffle(list);
+        list.forEach(System.out::println);
+    }
+}

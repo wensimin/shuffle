871e6b83bf984496e6356a86dcad2bad722ad076
wensimin <hukaizhiyu2@gmail.com>
2019-07-06 11:17:42 +0800

    完成

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Main {
 
-    private static final String[] data = {"a", "i", "u", "e", "o", "ka", "ki", "ku", "ke", "ko", "sa", "si", "su", "se", "so"};
+    private static final String[] data = {"a", "i", "u", "e", "o", "ka", "ki", "ku", "ke", "ko", "sa", "si", "su", "se", "so", "ta", "ti", "tu", "te", "to", "na", "ni", "nu", "ne", "no"};
 
     public static void main(String[] args) {
         List<String> list = Arrays.asList(data);

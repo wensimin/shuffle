86c8bfb1e8fb23ddc9af34fa92a130bf62d55979
wensimin <hukaizhiyu2@gmail.com>
2019-07-17 16:23:09 +0800

    ダタ

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+        import java.util.Collections;
+        import java.util.List;
 
 public class Main {
 
-    private static final String[] data = {"a", "i", "u", "e", "o", "ka", "ki", "ku", "ke", "ko", "sa", "si", "su", "se", "so", "ta", "ti", "tu", "te", "to", "na", "ni", "nu", "ne", "no"};
+    private static final String[] data = {"a", "i", "u", "e", "o", "ka", "ki", "ku", "ke", "ko", "sa", "si", "su", "se",
+            "so", "ta", "ti", "tu", "te", "to", "na", "ni", "nu", "ne", "no", "ha", "hi", "hu", "he", "ho", "ma", "mi",
+            "mu", "me", "mo", "ya", "yu", "yo", "ra", "ri", "ru", "re", "ro", "wa", "wo", "n"};
 
     public static void main(String[] args) {
         List<String> list = Arrays.asList(data);
